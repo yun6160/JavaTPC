f8ecc2e3ce275044d8a5e55717eda1306beb1589
yun6160 <devyun6160@gmail.com>
2021-09-30 19:20:54 +0900

    상속

--- src/main/java/kr/tpc/Cat.java
+++ src/main/java/kr/tpc/Cat.java
@@ -5,4 +5,9 @@ public class Cat extends Animal {
     public void night(){
         System.out.println("고양이는 밤에 잠을 자지 않는다");
     }
+
+    @Override
+    public void eat() {
+        System.out.println("고양이처럼 밥먹음");
+    }
 }
--- src/main/java/kr/tpc/Dog.java
+++ src/main/java/kr/tpc/Dog.java
@@ -2,6 +2,9 @@ package kr.tpc;
 
 public class Dog extends Animal {
     // 이름, 나이, 종: 상태정보
-
+    // 재정의 오버라이드
+    public void eat() {
+        System.out.println("개처럼 먹다");
+    }
 
 }
